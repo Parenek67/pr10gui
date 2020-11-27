353aa38fc0c86e160bb4834d35585e88a777d7b6
Parenek67 <kliment207@hgmail.com>
2020-11-28 01:21:50 +0300

    Initial commit

---
+++ src/com/company/ImageDocument.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class ImageDocument implements IDocument{
+    @Override
+    public String info() {
+        return "это изображение";
+    }
+}
---
+++ src/com/company/TextDocument.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class TextDocument implements IDocument{
+    @Override
+    public String info() {
+        return "это текстовый документ";
+    }
+
+}
